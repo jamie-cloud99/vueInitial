0380a377f473ebed39ca92972fc4b6ed7109ca8f
jamie-cloud99 <j1999j88@gmail.com>
2023-04-03 22:59:42 +0800

    新增基本檔案

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+  {{ name }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: process.env.VUE_APP_NAME
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap";
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <WordCard></WordCard>
+  </div>
+</template>
+
+<script>
+import WordCard from '@/components/WordCard.vue'
+export default {
+  components: { WordCard }
+}
+</script>
